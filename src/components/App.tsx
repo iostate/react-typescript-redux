3468017a53f0984a89c3c57eafc28ca8a7681e2d
iostate <qmtruong92@gmail.com>
2022-08-03 13:20:44 -0700

    add tailwind css

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,15 +2,29 @@ import { Provider } from 'react-redux';
 import { store } from "../state/store"
 import RepositoriesList from './RepositoriesList';
 
+type FreakAccidentProps = {
+  color: string;
+}
+
+const FreakAccident = ({ color }: FreakAccidentProps) => {
+  return (<h1 className={`text-${color}-500 rounded-lg p-6`}>Welcome to my blog about Redux!</h1>)
+}
+
 
 const App = () => {
+  const color = 'red';
   return (
     <>
+      {/* @ts-ignore:next-line */}
       <Provider store={store}>
         <div>
           <h1>Search For a Package</h1>
           <RepositoriesList />
         </div>
+        <div>
+          <FreakAccident color={color} />
+        </div>
+
       </Provider>
     </>
   );
